9f3972eb16de77bd1781619fbb6ea73b8f32aa08
Ilya Gorokhov <tairesh.rus@gmail.com>
2016-08-17 19:19:52 +0500

    start

--- config/db.php
+++ config/db.php
@@ -2,8 +2,6 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'sqlite:'.dirname(__FILE__).'/../database/database.sqlite',
     'charset' => 'utf8',
 ];
--- config/params.php
+++ config/params.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'tairesh.rus@gmail.com',
 ];
